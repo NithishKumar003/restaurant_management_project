f99180da653cf82b6f94b68eef5a190e91846387
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-08-04 10:12:01 +0530

    18829

--- home/views.py
+++ home/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+import requests
+from .models import MenuItem
 
 def homepage(request):
     return render(request, 'home/menu.html')
+
+def menu_view(request):
+    menu_items = MenuItem.objects.all()
+    return render(request, 'home/menu.html', {'menu_items': menu_items})

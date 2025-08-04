d534698a4befc4e9116d6d1ba222cc526d6ea33e
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-08-04 12:25:32 +0530

    18830

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import MenuItem, Order, OrderItem
 
-admin.site.register(MenuItem)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'available']
+    search_fields = ['name']
+
+@admin.register(Order):
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'total_amount', 'status', 'created_at']
+    list_filter = ['status', 'created_at']
+
+@admin.register(OrderItem):
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'menu_item', 'quantity']

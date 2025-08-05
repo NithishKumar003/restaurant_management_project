1e7b85f072f0706acf1d0d34a48f99606ae618bd
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-08-05 08:44:05 +0530

    18831

--- home/views.py
+++ home/views.py
@@ -8,3 +8,24 @@ def homepage(request):
 def menu_view(request):
     menu_items = MenuItem.objects.all()
     return render(request, 'home/menu.html', {'menu_items': menu_items})
+
+class MenuAPIView(APIView):
+    def get(self, request):
+        menu = [
+            {
+                "name":"Margherita Pizza",
+                "description": "Classic cheese and tomato pizza",
+                "price":250.00
+            },
+            {
+                "name": "Veg burger",
+                "description": "Loaded veg patty with lettuce and tomato",
+                "prive": 120.00
+            },
+            {
+                "name": "Pasta Alfredo", 
+                "description": "Creamy white sauce pasta with vegetables",
+                "price": "180.00"
+            }
+        ]
+        return Response(menu)

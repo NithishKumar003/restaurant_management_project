0c6e4245d20df782c8e38f22ff842ecbf26caa7e
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-08-05 10:21:26 +0530

    18833

--- home/forms.py
+++ home/forms.py
@@ -4,4 +4,14 @@ from .models import ContactSubmission
 class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactSubmission
-        fields = ['name', 'email']
+        fields = ['name', 'email', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Your Name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Your Email'}),
+            'message': forms.Textarea(attrs={'placeholder': 'Youe Message', 'rows': 4}),
+        }
+        labels = {
+            'name': 'Full Name', 
+            'email': 'Email Address', 
+            'message': 'Message',
+        }

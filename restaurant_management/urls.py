dd1fd652ed11e353f7c6e0fe802f7c69697aeb35
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-08-08 11:12:24 +0530

    26604

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.conf.urls import handler404
-from home.views import custom_404_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +25,4 @@ urlpatterns = [
     path('api/orders/',include('orders.urls')),
 ]
 
-handler404 = custom_404_view
+handler404 = 'home.views.custom_404_view'

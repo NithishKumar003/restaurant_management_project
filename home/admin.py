efa6b3eb4ef21ae8dd0a119243ea1c6d13574c77
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-08-08 11:17:24 +0530

    26604

--- home/admin.py
+++ home/admin.py
@@ -1,19 +1,5 @@
 from django.contrib import admin
-from .models import MenuItem, Order, OrderItem, ContactSubmission, UserProfile
-
-# @admin.register(MenuItem)
-# class MenuItemAdmin(admin.ModelAdmin):
-#     list_display = ['name', 'price', 'category']
-#     search_fields = ['name']
-
-# @admin.register(Order)
-# class OrderAdmin(admin.ModelAdmin):
-#     list_display = ['customer', 'total_amount', 'status', 'created_at']
-#     list_filter = ['status', 'created_at']
-
-# @admin.register(OrderItem)
-# class OrderItemAdmin(admin.ModelAdmin):
-#     list_display = ['order', 'menu_item', 'quantity']
+from .models import MenuItem, Order, OrderItem, ContactSubmission, UserProfile, RestaurantInfo
 
 admin.site.register(MenuItem)
 admin.site.register(Order)

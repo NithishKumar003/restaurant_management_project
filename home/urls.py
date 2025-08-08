b70a4fd6b97bf93ed94914041114536ee4375d75
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-08-08 08:24:28 +0530

    24883

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', homepage, name='home'),
-    path('api/menu/', MenuAPIView.as_view(), name='menu-api'),
+    path('api/menu/', views.menu_api, name='menu-api'),
     path('about/', views.about_page, name='about'),
 ]

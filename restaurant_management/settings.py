6b5d857e23e210cbfeab17c754810903162fdb23
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-08-08 08:46:50 +0530

    24884

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

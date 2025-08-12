ab39d340160e1d426fc84f038ca0996bb766c085
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-08-12 09:56:31 +0530

    29015

--- home/apps.py
+++ home/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = FeedbackForm
+        fields = ['name', 'comments']
+        widgets = {
+            'comments': forms.Textarea(attrs={'rows'; 4, 'placeholder': 'Write your feedback here....'}),
+        }

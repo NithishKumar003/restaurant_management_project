74931f6b04c5ea19f51d3df5f6f7f309ea84d7e2
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-08-12 09:52:33 +0530

    29015

--- home/models.py
+++ home/models.py
@@ -75,3 +75,11 @@ class RestaurantInfo(models.Model):
 
     def __str__(self):
         return self.name
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    comments = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Feedback from {self.name or 'Anonymous'} on {self.created_at.strftime('%Y-%m-%d')}"

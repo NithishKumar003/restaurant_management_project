b96f720a079a0eac9f34e40a2d934623bd358b84
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-08-13 11:23:14 +0530

    31683

--- home/views.py
+++ home/views.py
@@ -70,17 +70,8 @@ def menu_api(request):
 
     try:
         menu_items = MenuItem.objects.all()
-
-        data = [
-            {
-                "name": item.name,
-                "description": item.description,
-                "price": item.price,
-                "image": request.build_absolute_uri(item.image.url) if item.image else None
-            }
-            for item in menu_items
-        ]
-        return JsonResponse(data, safe=False)
+        serializer = MenuItemSerializer(menu_items, many=True, context={'request': request})
+        return Response(serializer.data)
 
     except DatabaseError as db_error:
         # Handle database-specific errors

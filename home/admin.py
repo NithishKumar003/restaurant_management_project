c8a90ee4bce23d91aa79a128784990f5fc2de8b7
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-08-13 09:55:15 +0530

    31679

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import MenuItem, Order, OrderItem, ContactSubmission, UserProfile, RestaurantInfo
+from .models import MenuItem, Order, OrderItem, ContactSubmission, UserProfile, RestaurantInfo, RestaurantLocation
 
 admin.site.register(MenuItem)
 admin.site.register(Order)
@@ -7,3 +7,4 @@ admin.site.register(OrderItem)
 admin.site.register(ContactSubmission)
 admin.site.register(UserProfile)
 admin.site.register(RestaurantInfo)
+admin.site.register(RestaurantLocation)

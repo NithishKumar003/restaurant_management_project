4d50bed2732117459b96015246e660a697b397a1
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-08-13 09:52:49 +0530

    31679

--- home/models.py
+++ home/models.py
@@ -68,10 +68,19 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"Profile Of {self.user.username}"   
 
-class RestaurantInfo(models.Model):
-    name = models.CharField(max_length=255)
+class RestaurantLocation(models.Model):
     address = models.TextField()
-    phone = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.address}, {self.city}, {self.state} - {self.zip_code}"
+
+class RestaurantInfo(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    location = models.OneToOneField(RestaurantLocation, on_delete=models.CASCADE, related_name="restaurant_info")
 
     def __str__(self):
         return self.name

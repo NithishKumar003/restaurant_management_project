deb35c8019f627c54212c7ef184f50e44c0d24ed
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-08-13 11:20:44 +0530

    31683

--- home/serializers.py
+++ home/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models import MenuItem
 
 class MenuItemSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = MenuItem
-        fields = ['name', 'description', 'price']
+        fields = ['name', 'description', 'price', 'image']
+
+    def get_image(self, obj):
+        request = self.context.get('request')
+        if obj.image:
+            return request.build_absolute_uri(obj.image.url)
+        return None

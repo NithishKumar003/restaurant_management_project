3698b9ec3b4239d9e1f01cbbd56b1d6971c75d2b
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-08-13 10:19:50 +0530

    31681

--- home/models.py
+++ home/models.py
@@ -7,6 +7,7 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.CharField(max_length=50)
     available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
 
     def __str__(self):
         return self.name

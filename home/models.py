e4afd1ba01057590cd0dec74a59f9a934341707c
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-08-22 09:01:17 +0530

    36855

--- home/models.py
+++ home/models.py
@@ -95,3 +95,13 @@ class Feedback(models.Model):
 
     def __str__(self):
         return f"Feedback from {self.name or 'Anonymous'} on {self.created_at.strftime('%Y-%m-%d')}"
+
+
+class Special(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name

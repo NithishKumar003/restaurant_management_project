7179596e299f769f61c7e4818705594a1d1aedeb
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-08-22 08:29:42 +0530

    36857

--- home/urls.py
+++ home/urls.py
@@ -13,10 +13,10 @@ urlpatterns = [
     path('menu/add/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
     path('faq/', views.faq, name='faq'),
     path('login/', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    paath('logout/', auth_views.LogoutViews.as_view(next_page="homepage"), name="logout"),
+    path('logout/', auth_views.LogoutViews.as_view(next_page="homepage"), name="logout"),
     path('order/', views.order_page, name="order_page"),
     path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
-    path('order-confirmation/', views.order_confirmation, name='order_confirmation'),
+    path('order-confirmation/', views.order_confirmation, name='order_confirmation')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

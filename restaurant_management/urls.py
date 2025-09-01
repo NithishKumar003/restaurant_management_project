9dffbd0f06a3f963ed42f0392e47224ad532f5d9
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-09-01 08:12:07 +0530

    41237

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('api/orders/',include('orders.urls')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 handler404 = 'home.views.custom_404_view'

d79cfba8917ea725dcb28cc47cdeeef0ddc4f332
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-09-24 13:54:51 +0530

    49881

--- home/models.py
+++ home/models.py
@@ -134,3 +134,11 @@ class Chef(models.Model):
 
     def __str__(self):
         return self.name
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    discount = models.DecimalField(max_digits=5, decimal_places=2, help_text = "Discount percentage")
+    active = models.BooleanField(defaul=True)
+
+    def __str__(self):
+        return f"{self.code} - {self.discount}%"

aef9f18d9b89ec198418f9b93ca71f8133969e08
NITHISH KUMAR E <enithishkumar91@gmail.com>
2025-09-24 13:35:56 +0530

    49879

--- home/models.py
+++ home/models.py
@@ -53,6 +53,12 @@ class OrderItem(models.Model):
         super().save(*args, **kwargs)
         self.order.update_total()
 
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class ContactSubmission(models.Model):
     name = models.CharField(max_length=100)
     email  = models.EmailField()
